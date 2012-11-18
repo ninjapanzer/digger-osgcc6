6688febf2e687dbdd518cfeef34519a293e89bc9
Julian <juliantoker@gmail.com>
2012-11-18 00:04:25 -0500

    Adding Finished player thing.

--- javascript/player.js
+++ javascript/player.js
@@ -9,12 +9,16 @@ var Player = exports.Player = function(initialLocation) {
       if (event.type === gamejs.event.KEY_DOWN) {
          if (event.key === gamejs.event.K_LEFT) {
             this.rect.x -= globals.tileSize[0];
+            this.image = gamejs.image.load('assets/images/player/player_left.png');
          } else if (event.key === gamejs.event.K_RIGHT) {
             this.rect.x += globals.tileSize[0];
+            this.image = gamejs.image.load('assets/images/player/player_right.png');
          } else if (event.key === gamejs.event.K_DOWN) {
             this.rect.y += globals.tileSize[0];
+            this.image = gamejs.image.load('assets/images/player/player_down.png');
          } else if (event.key === gamejs.event.K_UP) {
             this.rect.y -= globals.tileSize[0];
+            this.image = gamejs.image.load('assets/images/player/player_up.png');
          }
       }
     };
