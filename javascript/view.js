c94c3394cab428e97a8f7a70976cfa9bed0032bd
Paul Scarrone <paul.scarrone@gmail.com>
2012-11-18 09:59:54 -0500

    Added total Score and Headings to HUD

--- javascript/view.js
+++ javascript/view.js
@@ -12,20 +12,36 @@ var HUD = exports.HUD = function(){
       draw.rect(surface, '#fff000', this.HUDBorder, 4);
    };
    this.update = function(player, display){
-   	var position = [20,30];
+   	//Upper Start
+   	var upperPosition = [10, 5];
+   	//Lower Start
+   	var position = [10,30];
+   	var font = new gamejs.font.Font('20px monospace');
+
+   	//Upper Level
+   		var headingSurface = font.render('Resources', '#ffffff');
+   		display.blit(headingSurface, [10, 5]);
+   		var upgradeHeadingSurface = font.render('Upgrades', '#ffffff');
+   		display.blit(upgradeHeadingSurface,[500,5])
+   		var scoreHeadingSurface = font.render('Score', '#ffffff');
+   		display.blit(scoreHeadingSurface, [globals.screenDim[0]-(scoreHeadingSurface.getSize()[0]+10),5]);
+
+   	var scoreTotal = 0;
    	player.inventory.forEach(function(item){
    		item.rect.x = position[0];
    		item.rect.y = position[1];
    		item.draw(display);
-   		var font = new gamejs.font.Font('20px monospace');
+   		//Lower Level
+   		scoreTotal += item.count*item.value;
    		var resourceCountSurface = font.render(item.count, '#ffffff');
-   		var headingSurface = font.render('Resources', '#fffffff');
    		var messageSize = resourceCountSurface.getSize();
-   		display.blit(headingSurface, [10, 5]);
    		position[0] =  position[0] + 11;
    		display.blit(resourceCountSurface, position);
    		position[0] = position[0] + messageSize[0]+11;
    	});
+
+   	var scoreValueSurface = font.render(scoreTotal*60000*50*50*100, '#ffffff');
+   	display.blit(scoreValueSurface, [globals.screenDim[0]-(scoreValueSurface.getSize()[0]+10),30]);
    }
    return this;
 };
