bd77e7dada68c62ce4e543ff0fca3bdb73ed3417
Paul Scarrone <paul.scarrone@gmail.com>
2012-11-18 09:43:37 -0500

    Started adding HUD Headers

--- javascript/view.js
+++ javascript/view.js
@@ -4,23 +4,27 @@ var draw = require('gamejs/draw');
 var font = require('gamejs/font');
 
 var HUD = exports.HUD = function(){
-   this.rect = new gamejs.Rect([0,0], [globals.screenDim[1],50]);
+   this.HUD = new gamejs.Rect([0,0], [globals.screenDim[0],50]);
+   this.HUDBorder = new gamejs.Rect([0,0], [globals.screenDim[0],50]);
    this.color = '#000000'
    this.draw = function(surface){
-      draw.rect(surface, this.color, this.rect, 0);
+      draw.rect(surface, this.color, this.HUD, 0);
+      draw.rect(surface, '#fff000', this.HUDBorder, 4);
    };
    this.update = function(player, display){
-   	var position = [100,25];
+   	var position = [20,30];
    	player.inventory.forEach(function(item){
    		item.rect.x = position[0];
    		item.rect.y = position[1];
    		item.draw(display);
    		var font = new gamejs.font.Font('20px monospace');
-   		var helloSurface = font.render(item.count, '#ffffff');
-   		var messageSize = helloSurface.getSize();
+   		var resourceCountSurface = font.render(item.count, '#ffffff');
+   		var headingSurface = font.render('Resources', '#fffffff');
+   		var messageSize = resourceCountSurface.getSize();
+   		display.blit(headingSurface, [10, 5]);
    		position[0] =  position[0] + 11;
-   		display.blit(helloSurface, position);
-   		position[0] = position[0] + 55;
+   		display.blit(resourceCountSurface, position);
+   		position[0] = position[0] + messageSize[0]+11;
    	});
    }
    return this;
